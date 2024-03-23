3942819dc3e0b4835ad13b5e352d6dbebd70fd35
Eric Johnson <eric.j.johnson2@gmail.com>
2024-03-23 11:25:23 -0500

    removed app folder, running this in jupyter instead of js

--- app/static/js/logic.js
+++
@@ -1,9 +0,0 @@
-// assign the local url to a constant variable
-const url = "http://127.0.0.1:5000/api/location"
-const url2 = "http://127.0.0.1:5000/api/nutrition"
-// const summaryURL = "http://127.0.0.1:5000/api/summary";
-
-// ****************
-// DROPDOWN MENU
-// ****************
-
